eabd4a053807a14f94ef8ca0b4a02f03692bc5ed
gwindlord <gwindlord@buildserver>
2016-01-16 10:50:18 +0100

    + Added system package mode to build within ROM (its scripts will take care about the build itself); - Removed translation tick as I could not get it working (suspect it is disabled in core blobs); * Fixed non-working filter URLs in WebRefiner

--- build/make.sh
+++ build/make.sh
@@ -3,6 +3,8 @@
 LOCAL_REPO="$(dirname $(dirname $(readlink -f $0)))"
 ROMName=$(basename $LOCAL_REPO)
 
+isSystem="$1"
+
 cd $LOCAL_REPO/src
 
 # this commit is here because "gclient sync -n --no-nag-max" changes some files and they need to be either reset or committed to make repo clean
@@ -16,11 +18,21 @@ git apply $LOCAL_REPO/build/patches/build-deps.patch && git add -f $(git status 
 # some of them are part of other commits, so had to use patching
 git apply $LOCAL_REPO/build/patches/signin.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Getting sign-in back"
 
+if [[ "$isSystem" != "--system" ]];
+then
+  git apply $LOCAL_REPO/build/patches/themes.patch
+else
+  # stay Browser when building as system package
+  git apply $LOCAL_REPO/build/patches/themes_system.patch
+fi
 # I do not know other way to get it themed, sorry
-git apply $LOCAL_REPO/build/patches/themes.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Masking to Chrome Beta for themes support :->"
+git add -f $(git status -s | awk '{print $2}') && git commit -m "Masking to Chrome Beta for themes support :->"
 
 #cp -rf $LOCAL_REPO/build/icons/res $LOCAL_REPO/src/swe/channels/default/
 #git add -f $(git status -s | awk '{print $2}') && git commit -m "Shamelessly stealing icons from Slim >_<"
+
+# removing Google Translate tick as it does not work anyway
+git apply $LOCAL_REPO/build/patches/remove_translate.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Remove page translation tick"
 
 cp -f $LOCAL_REPO/build/webrefiner_conf/web_refiner_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
 cp -f $LOCAL_REPO/build/webdefender_conf/web_defender_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
@@ -35,4 +47,10 @@ gclient runhooks -v
 # implementing custom translated lines build
 patch -p0 < $LOCAL_REPO/build/patches/chrome_strings_grd_ninja.diff
 
-$LOCAL_REPO/build/run.sh &
+# for ROM build env - to allow it starting system package build itself
+if [[ "$isSystem" != "--system" ]];
+then
+
+  $LOCAL_REPO/build/run.sh &
+
+fi
