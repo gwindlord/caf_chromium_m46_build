b583a947a1c3596bf89e84088813acc8d7f99adb
gwindlord <gwindlord@buildserver>
2016-01-30 10:20:34 +0100

    + Added translation for WebRefiner and WebDefender + Added more sources for WebDefender * Changed translation patch according to recent commits

--- build/make.sh
+++ build/make.sh
@@ -9,6 +9,14 @@ isCustom="$1"
 
 cd $LOCAL_REPO/src
 
+# this is because they placed WebRefiner and WebDefender translation into zip archive >_<
+pushd $LOCAL_REPO/src/components/web_refiner/java/
+  cp -rf $LOCAL_REPO/build/webrefiner/values-ru .
+  zip -0TX libswewebrefiner_java.zip values-ru/strings.xml
+  rm -rf values-ru/
+  git add -f $(git status -s | awk '{print $2}') && git commit -m "Adding WebRefiner and WebDefender translation"
+popd
+
 # this commit is here because "gclient sync -n --no-nag-max" changes some files and they need to be either reset or committed to make repo clean
 git add -f $(git status -s | awk '{print $2}') && git commit -m "Dummy"
 
@@ -23,7 +31,7 @@ git apply $LOCAL_REPO/build/patches/themes.patch && git add -f $(git status -s |
 # removing Google Translate tick as it does not work anyway
 git apply $LOCAL_REPO/build/patches/remove_translate.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Remove page translation tick"
 
-cp -f $LOCAL_REPO/build/webrefiner_conf/web_refiner_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
+cp -f $LOCAL_REPO/build/webrefiner/web_refiner_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
 cp -f $LOCAL_REPO/build/webdefender_conf/web_defender_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
 git add -f $(git status -s | awk '{print $2}') && git commit -m "Shamelessly stealing WebRefiner and WebDefender configs from JSwarts and extending them"
 
