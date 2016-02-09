4deb5eaed33895443fa97fe7125acdb4a871dffa
gwindlord <gwindlord@buildserver>
2016-02-09 11:05:57 +0100

    + Added revert of 5b20b729a33916a315e03f2be7be8edcca7bd27e - I like edge navigation in low-power mode * Changes according to recent strings commits (SWE strings finally externalized as resources) * WebDefender config moved to WebRefiner (no idea where 4ba5b9882bb2fe4b7ae70d96d235403d1575b9bf moves it actually)

--- build/make.sh
+++ build/make.sh
@@ -12,13 +12,17 @@ cd $LOCAL_REPO/src
 # this is because they placed WebRefiner and WebDefender translation into zip archive >_<
 pushd $LOCAL_REPO/src/components/web_refiner/java/
   cp -rf $LOCAL_REPO/build/webrefiner/values-ru .
-  zip -0TX libswewebrefiner_java.zip values-ru/strings.xml
-  rm -rf values-ru/
+  cp -rf $LOCAL_REPO/build/webrefiner/raw .
+  zip -0TX libswewebrefiner_java.zip values-ru/strings.xml raw/web_defender_configuration.txt
+  rm -rf values-ru/ raw/
   git add -f $(git status -s | awk '{print $2}') && git commit -m "Adding WebRefiner and WebDefender translation"
 popd
 
 # this commit is here because "gclient sync -n --no-nag-max" changes some files and they need to be either reset or committed to make repo clean
 git add -f $(git status -s | awk '{print $2}') && git commit -m "Dummy"
+
+# revert "Disable edge navigation in low-power mode" - I want it! :)
+git revert 5b20b729a33916a315e03f2be7be8edcca7bd27e
 
 # reverting Google sign-in and extended bookmarks related removals
 # (well, they are not removed but placed under ENABLE_SUPPRESSED_CHROMIUM_FEATURES flag, but this flag is not added for actual usage)
@@ -32,8 +36,8 @@ git apply $LOCAL_REPO/build/patches/themes.patch && git add -f $(git status -s |
 git apply $LOCAL_REPO/build/patches/remove_translate.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Remove page translation tick"
 
 cp -f $LOCAL_REPO/build/webrefiner/web_refiner_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
-cp -f $LOCAL_REPO/build/webdefender_conf/web_defender_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
-git add -f $(git status -s | awk '{print $2}') && git commit -m "Shamelessly stealing WebRefiner and WebDefender configs from JSwarts and extending them"
+#cp -f $LOCAL_REPO/build/webdefender_conf/web_defender_conf $LOCAL_REPO/src/chrome/android/java/res_chromium/raw/
+git add -f $(git status -s | awk '{print $2}') && git commit -m "Shamelessly stealing WebRefiner config from JSwarts and extending it"
 
 # reverting to old bookmarks UI - have to change strategy due to 9fd8eb1f1374a51f048ec255f8e341ff2e381234
 git apply $LOCAL_REPO/build/patches/bookmarks.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Reverting to old bookmarks UI"
@@ -41,6 +45,8 @@ git revert 2f8a15af8865836a98c578138dc7f59e1b043cf7 || git add -f $(git status -
 
 # media files saving and exit dialog are disabled by default
 git apply $LOCAL_REPO/build/patches/qrd_features.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Enable QRD features"
+
+git apply $LOCAL_REPO/build/patches/swe_strings.patch && git add -f $(git status -s | awk '{print $2}') && git commit -m "Adding SWE translations"
 
 if [[ "$isCustom" != "--no-gclient" ]];
 then
